12ea163e374fad6023b2ab5b04bd568c628ca41f
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-01 20:58:30 -0700

    Fixing windows script

--- scripts/dependencies-windows.sh
+++ scripts/dependencies-windows.sh
@@ -4,6 +4,6 @@ choco upgrade chocolatey
 choco install gcc
 choco install cmake
 choco install python3
-dir C:\Python37
+dir C:/Python37
 # git clone https://github.com/pypa/get-pip.git
 # python3 ./get-pip/get-pip.py
