94b8c99ce983fb39d70e9ceae1ca486ca3f4f981
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-02 06:07:40 -0700

    Fixing conan

--- scripts/dependencies-windows.sh
+++ scripts/dependencies-windows.sh
@@ -5,4 +5,4 @@ choco install python3
 git clone https://github.com/pypa/get-pip.git
 C:/Python37/python ./get-pip/get-pip.py
 export PATH=c:/python37:c:/python37/Scripts:$PATH
-C:/Python37/Scripts/pip install conan
+pip install conan
