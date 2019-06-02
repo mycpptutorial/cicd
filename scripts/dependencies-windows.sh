dd4a475ccce68a6ada0e55895d2e17f07a4ccb5f
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-02 11:53:52 -0700

    Fixing conan

--- scripts/dependencies-windows.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-choco upgrade chocolatey
-choco install python3
-git clone https://github.com/pypa/get-pip.git
-C:/Python37/python ./get-pip/get-pip.py
-SET PATH=c:/python37;c:/python37/Scripts;$PATH
-pip install conan
