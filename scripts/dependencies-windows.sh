6a783c31e87efb831d4853c6baf18b41ef3e5895
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-02 23:07:30 -0400

    Fix C++ compiler

--- scripts/dependencies-windows.sh
+++ scripts/dependencies-windows.sh
@@ -9,5 +9,10 @@ export PATH=/c/python37:/c/python37/Scripts:/c/python37/lib/site-packages:$PATH
 python --version
 python ./get-pip/get-pip.py
 pip --version
+export TRAVIS_COMPILER=gcc
+export CXX=gcc
+export CXX_FOR_BUILD=gcc
+export CC=gcc
+export CC_FOR_BUILD=gcc
 pip install conan
 conan --version
