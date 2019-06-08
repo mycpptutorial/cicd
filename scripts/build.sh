9eac4ef3b495b8956c3683700d7bcb61c978d7e3
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-07 23:45:43 -0400

    Added export vars to c compiter

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,12 @@
 
 export PATH=/c/python37:/c/python37/Scripts:/c/python37/lib/site-packages:$PATH
 
+export TRAVIS_COMPILER=gcc
+export CXX=gcc
+export CXX_FOR_BUILD=gcc
+export CC=gcc
+export CC_FOR_BUILD=gcc
+
 CONAN_SOURCE=$1
 BUILD_FOLDER=$2
 
