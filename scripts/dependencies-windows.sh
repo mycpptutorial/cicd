60346e5fd856c138fa90aca969381581f6de483f
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-08 00:05:21 -0400

    Changed compiler from gcc to g++

--- scripts/dependencies-windows.sh
+++ scripts/dependencies-windows.sh
@@ -9,10 +9,10 @@ export PATH=/c/python37:/c/python37/Scripts:/c/python37/lib/site-packages:$PATH
 python --version
 python ./get-pip/get-pip.py
 pip --version
-export TRAVIS_COMPILER=gcc
-export CXX=gcc
-export CXX_FOR_BUILD=gcc
-export CC=gcc
-export CC_FOR_BUILD=gcc
+export TRAVIS_COMPILER=g++
+export CXX=g++
+export CXX_FOR_BUILD=g++
+export CC=g++
+export CC_FOR_BUILD=g++
 pip install conan
 conan --version
