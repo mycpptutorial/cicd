bdd4889ef8a2b65fd0263d1c5825f56ce0a8a695
Nasim Kabiliravi <kabiliravi@gmail.com>
2019-06-15 00:01:44 -0400

    Added conan scripts to publish to bintray

--- scripts/publish-artifact.sh
+++ scripts/publish-artifact.sh
@@ -6,3 +6,7 @@ fi
 
 conan remote add datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable https://api.bintray.com/conan/mycpptutorial/datetimeutil
 conan user -p $BINTRAY_PASSWORD -r datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable $BINTRAY_USERNAME
+
+conan export . datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable
+conan install datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable --build=datetimeutil-shared-lib-conan
+conan upload "datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable" -r datetimeutil-shared-lib-conan/1.0.0@mycpptutorial/stable --all
